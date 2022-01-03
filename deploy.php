0e7bd18e0d6c6297b7a42a14d8f940cb00f528d2
Alec Breton <aleccbreton@gmail.com>
2022-01-03 17:52:55 -0500

    [ab] Minimize deploy script

--- deploy.php
+++ deploy.php
@@ -1,11 +1,5 @@
 <?php
-    $command = 'eval $(ssh-agent -s) ssh-add ~/.ssh/id_rsa_alecbreton && git pull 2>&1';
-
-    $output = '';
-    $tmp = shell_exec($command);
-
-    $output .= "<span style=\"color: #6BE234;\">\$</span><span style=\"color: #729FCF;\">{$command}\n</span><br />";
-    $output .= htmlentities(trim($tmp)) . "\n<br /><br />";
+    $tmp = shell_exec('eval $(ssh-agent -s) && ssh-add ~/.ssh/id_rsa_alecbreton && git pull 2>&1');
 ?>
 
 <!DOCTYPE HTML>
@@ -15,8 +9,6 @@
         <title>Git Deployment Script</title>
     </head>
     <body>
-        <div>
-            <?php echo $output; ?>
-        </div>
+        $<?php echo htmlentities(trim($tmp)); ?>
     </body>
 </html>
